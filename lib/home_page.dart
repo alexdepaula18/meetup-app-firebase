3da9404d2792f4b4e922b2ae7fe5275e378e6719
Alex de Paula <alex.paula@decodev.com.br>
2023-03-21 14:18:12 -0300

    Adicionando os arquivo para o macos e novos widgets do aplicativo

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -2,9 +2,16 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+import 'package:firebase_auth/firebase_auth.dart'
+    hide EmailAuthProvider, PhoneAuthProvider;
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
+import 'app_state.dart';
+import 'guest_book.dart';
+import 'src/authentication.dart';
 import 'src/widgets.dart';
+import 'yes_no_selection.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
@@ -21,6 +28,14 @@ class HomePage extends StatelessWidget {
           const SizedBox(height: 8),
           const IconAndDetail(Icons.calendar_today, 'October 30'),
           const IconAndDetail(Icons.location_city, 'San Francisco'),
+          Consumer<ApplicationState>(
+            builder: (context, appState, _) => AuthFunc(
+                loggedIn: appState.loggedIn,
+                signOut: () {
+                  FirebaseAuth.instance.signOut();
+                }
+            ),
+          ),
           const Divider(
             height: 8,
             thickness: 1,
@@ -32,6 +47,31 @@ class HomePage extends StatelessWidget {
           const Paragraph(
             'Join us for a day full of Firebase Workshops and Pizza!',
           ),
+
+          Consumer<ApplicationState>(
+            builder: (context, appState, _) => Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                if (appState.attendees >= 2)
+                  Paragraph('${appState.attendees} people going')
+                else if (appState.attendees == 1)
+                  const Paragraph('1 person going')
+                else
+                  const Paragraph('No one going'),
+                if(appState.loggedIn) ... [
+                  YesNoSelection(
+                    state: appState.attending,
+                    onSelection: (attending) => appState.attending = attending,
+                  ),
+                  const Header('Discussion'),
+                  GuestBook(
+                      addMessage: (message) => appState.addMessageToGuestBook(message),
+                      messages: appState.guestBookMessages,
+                  )
+                ]
+              ],
+            ),
+          )
         ],
       ),
     );
